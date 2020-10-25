812a5ad5177fdfcd910e7167ee55344f32585257
Jin-devST <31203142+Jin-dev92@users.noreply.github.com>
2020-10-26 00:00:05 +0900

    코드 리펙토링

--- src/main/java/com/devJin/grid/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/devJin/grid/springboot/config/auth/LoginUserArgumentResolver.java
@@ -1,5 +1,7 @@
 package com.devJin.grid.springboot.config.auth;
 
+import com.devJin.grid.springboot.config.auth.dto.LoginUser;
+import com.devJin.grid.springboot.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,8 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Component
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
@@ -15,12 +19,18 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     *   조건에 맞는 경우 메소드가 있다면 HandlerMethodArgumentResolver의 구현체가 지정한 값으로
     *   해당 메소드의 파라미터로 넘길수 있습니다.
     * */
+    private final HttpSession httpSession;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         /*  컨트롤러 메서드의 특정 파라미터를 지원하는지 판단
         *   여기서는 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인경우 true를 반환합니다.
         * */
-        return false;
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType()); // class.class -> 클래스 자체의 객체를 뽑아낸다.
+
+        return isLoginUserAnnotation && isUserClass;
     }
 
     @Override
@@ -28,6 +38,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
         /*  파라미터에 전달할 객체를 생성합니다.
         *   여기서는 세션에서 객체를 가져옵니다.
         * */
-        return null;
+        return httpSession.getAttribute("user");
     }
 }
